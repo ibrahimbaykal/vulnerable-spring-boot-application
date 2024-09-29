3cf1d5a74030528bdfa12dbf21c8960c21b7c05d
ibrahim.baykal <ibrahim.baykal@iys.org.tr>
2024-09-29 20:52:20 +0300

    add row for test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,5 @@ INSERT INTO PUBLIC.PROVIDERS (id, first_name, last_name, phone, street_address, 
 VALUES
 	(1,'Stephen','Milner','410-333-3333','101 Main St','Baltimore','MD','21230','Internal Medicine',TRUE,FALSE,FALSE),
 	(2,'Jane','Owens','410-333-3333','102 Main St','Baltimore','MD','21230','General Practice',TRUE,FALSE,TRUE),
-	(3,'Alfred','Newman','410-333-3333','103 Main St','Baltimore','MD','21230','Ear, Nose, and Throat',FALSE,TRUE,FALSE);
+	(3,'Alfred','Newman','410-333-3333','103 Main St','Baltimore','MD','21230','Ear, Nose, and Throat',FALSE,TRUE,FALSE),
+	(4,'ibrahim','ibrahim','410-333-3333','103 Main St','Baltimore','MD','21230','Ear, Nose, and Throat',FALSE,TRUE,FALSE);
